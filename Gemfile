3b0c847a4246c9823cda47f89d2a6773513dfa8c
DylanLawless <dylan.lawless@epfl.ch>
2022-01-24 19:22:45 +0100

    README test

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+ruby '2.7.0'
+gem "jekyll"
+gem "github-pages", group: :jekyll_plugins
+gem 'jekyll-seo-tag'
+gem 'jemoji'
+gem 'jekyll-sitemap'
+gem "webrick", "~> 1.7"
---
+++ _site/jek.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# to build sitemap with base url defined in _configyml instead of local host, run build, and then serve.
+# bundle exec jekyll build &
+
+# to serve locally (this will produce the incorrect sitemap again)
+bundle exec jekyll serve &
+sleep 3
+
+open -a Safari http://127.0.0.1:4000
+
